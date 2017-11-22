9e2f04b32edda7a853eee579dcbbc28db6f48470
dperrymorrow <dperrymorrow@gmail.com>
2017-11-22 15:34:55 -0800

    line highlighing

--- _source/public/scripts/app.js
+++ _source/public/scripts/app.js
@@ -6,3 +6,19 @@ document.querySelector(".hamburger").addEventListener("click", function(event) {
 Array.from(document.querySelectorAll(".blog-content a")).forEach(function(link) {
   link.setAttribute("target", "_blank");
 });
+
+Array.from(document.querySelectorAll("pre")).forEach(function(block) {
+  var lines = [];
+  block.innerHTML = block.innerHTML
+    .split("\n")
+    .map(function(line, index) {
+      if (line.startsWith("^")) {
+        lines.push(index + 1);
+        line = line.replace("^", "  ");
+      }
+      return line;
+    })
+    .join("\n");
+
+  block.setAttribute("data-line", lines.join(","));
+});
